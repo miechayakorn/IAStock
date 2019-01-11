3f61bee866068df896d8f148dcab463bbbc95972
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2019-01-11 19:19:21 +0700

    [Add Filter]

---
+++ IAStock/src/java/ia/Filter/CheckYearSessionFilter.java
@@ -0,0 +1,40 @@
+package ia.Filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+public class CheckYearSessionFilter implements Filter {
+
+    private FilterConfig filterConfig;
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest)request).getSession(false);
+        if(session == null || session.getAttribute("year") == null){
+            filterConfig.getServletContext().getRequestDispatcher("/Home").forward(request, response);
+            return;
+        }else{
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
